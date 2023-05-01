a287a0d430392e6f7f5a00db71964bbc34bec638
Rodrigo Oliveira <rodrigocode4@gmail.com>
2023-05-01 20:09:42 -0300

    chore: change path root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './src'),
     },
   },
   envDir: './env/',
