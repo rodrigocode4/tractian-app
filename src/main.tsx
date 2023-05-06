5e085d2c6bf64304c091fb6f179fc779bfb477c6
Rodrigo Oliveira <rodrigocode4@gmail.com>
2023-05-05 23:19:53 -0300

    refactor: remove msw

--- src/main.tsx
+++ src/main.tsx
@@ -4,33 +4,17 @@ import { createRoot } from 'react-dom/client'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import { App } from '~/modules/app'
 
-// import './main.css'
-
 if (import.meta.env.MODE === 'production') {
   disableReactDevTools()
 }
 
-const root = createRoot(document.getElementById('root') as HTMLElement)
+const root = createRoot(document.getElementById('root')!)
 
-if (import.meta.env.MODE === 'development') {
-  import('~/infrastructure/__mocks__/browser')
-    .then(({ worker }) => {
-      worker.start()
-    })
-    .then(() => {
-      root.render(
-        <StrictMode>
-          <App />
-        </StrictMode>,
-      )
-    })
-} else {
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
-}
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
 // Uncomment if you want to see the Lighthouse report in the console
 // import reportWebVitals from './reportWebVitals'
